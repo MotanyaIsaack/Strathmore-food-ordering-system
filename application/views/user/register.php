3a43f2cb5c772eea8b8e096011e4838b4f9a6018
Isaack Motanya <isaackmotanya6@gmail.com>
2018-09-26 15:05:28 +0300

    Updated Register File

---
+++ application/views/user/register.php
@@ -0,0 +1,56 @@
+
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper auth p-0 theme-two">
+        <div class="row d-flex align-items-stretch">
+          <div class="col-md-4 banner-section d-none d-md-flex align-items-stretch justify-content-center">
+            <div class="slide-content bg-2">
+            </div>
+          </div>
+          <div class="col-12 col-md-8 h-100 bg-white">
+            <div class="auto-form-wrapper d-flex align-items-center justify-content-center flex-column">
+              <div class="nav-get-started">
+                <p class="font">Already have an account?</p>
+                <a class="btn get-started-btn" href="<?php echo base_url()?>">SIGN IN</a>
+              </div>
+              <form action="#">
+              <h3 class="mr-auto font">Are you tired of standing in queues waiting for your meal?
+                 <br> <span class='get-started'>Then ,let's get started</span></h3><br>
+
+                <!-- <p class="mb-5 mr-auto font">Then lets get started.</p> -->
+                <div class="form-group">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="mdi mdi-account-outline"></i></span>
+                    </div>
+                    <input style="font-size:15px;font-family: 'Raleway', sans-serif;" type="text" class="form-control" placeholder="Email">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="mdi mdi-account-outline"></i></span>
+                    </div>
+                    <input style="font-size:15px;font-family: 'Raleway', sans-serif;" type="text" class="form-control" placeholder="Username">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="mdi mdi-lock-outline"></i></span>
+                    </div>
+                    <input style="font-size:15px;font-family: 'Raleway', sans-serif;" type="password" class="form-control" placeholder="Password">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="mdi mdi-lock-outline"></i></span>
+                    </div>
+                    <input style="font-size:15px;font-family: 'Raleway', sans-serif;" type="password" class="form-control" placeholder="Confirm Password">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <button class="btn btn-primary submit-btn">SIGN IN</button>
+                </div>
+                
