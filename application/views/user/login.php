54a12724313f8450659557c306756b7d13245556
Isaack Motanya <isaackmotanya6@gmail.com>
2018-09-26 15:05:14 +0300

    Updated Login page

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,5 +1,5 @@
 
-  <div class="container-scroller" style="font-family: 'Raleway', sans-serif;">
+  <div class="container-scroller">
     <div class="container-fluid page-body-wrapper full-page-wrapper">
       <div class="content-wrapper auth p-0 theme-two">
         <div class="row d-flex align-items-stretch">
@@ -10,18 +10,19 @@
           <div class="col-12 col-md-8 h-100 bg-white">
             <div class="auto-form-wrapper d-flex align-items-center justify-content-center flex-column">
               <div class="nav-get-started">
-                <p>Don't have an account?</p>
-                <a class="btn get-started-btn" href="register-2.html">GET STARTED</a>
+                <p class="font">Don't have an account?</p>
+                <a class="btn get-started-btn font" href="<?php echo base_url()?>users/register">GET STARTED</a>
               </div>
               <form action="#">
-                <h3 class="mr-auto">Hello! let's get started</h3>
-                <p class="mb-5 mr-auto">Enter your details below.</p>
+                <h3 class="mr-auto font">Welcome to the SU Food Ordering System, where ordering meals is made much faster.
+                  <br> <span class='get-started'></span></h3><br>
+                <!-- <p class="mb-5 mr-auto font details">Enter your details below.</p> -->
                 <div class="form-group">
                   <div class="input-group">
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="mdi mdi-account-outline"></i></span>
                     </div>
-                    <input type="text" class="form-control" placeholder="Username">
+                    <input style="font-size:15px;font-family: 'Raleway', sans-serif;" type="text" class="form-control" placeholder="Username">
                   </div>
                 </div>
                 <div class="form-group">
@@ -29,7 +30,7 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="mdi mdi-lock-outline"></i></span>
                     </div>
-                    <input type="password" class="form-control" placeholder="Password">
+                    <input style="font-size:15px;font-family: 'Raleway', sans-serif;" type="password" class="form-control input" placeholder="Password">
                   </div>
                 </div>
                 <div class="form-group">
