5d65a6d576874d67a16fe925e0ad140b2c3b4323
Isaack Motanya <isaackmotanya6@gmail.com>
2018-09-26 15:04:33 +0300

    Updated User Controller

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -9,22 +9,11 @@
 			$this->load->view('templates/header');
 			$this->load->view('user/login');
 			$this->load->view('templates/footer');
-			//Communicates with the user model
-			//Manual Loading the model
-			// $this->load->model('user_model');
-
-			// $data['results'] = $this->user_model->get_users();
-
-			
-			// $this->load->view('user_view',$data);
-
-			// foreach ($result as $object) {
-			// 	echo $object->id;
-			// 	echo " ";
-			// 	echo $object->username;
-			// 	echo " ";
-			// 	echo $object->password;
-			// }
+		}
+		public function register(){
+			$this->load->view('templates/header');
+			$this->load->view('user/register');
+			$this->load->view('templates/footer');
 		}
 	}
 	
