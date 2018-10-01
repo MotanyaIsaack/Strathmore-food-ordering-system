6920fdcda7d5122b067c652a5dce37dac8bdc8e3
Naym0 <38431892+Naym0@users.noreply.github.com>
2018-10-02 02:49:02 +0300

    Add files via upload
    A new branch with fully functional registration and login.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,7 +9,7 @@
   <title>SU Food Ordering System</title>
   <!-- plugins:css -->
   <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/vendor/css/main.css"/>
+  
   <link rel="stylesheet" href="<?php echo base_url()?>assets/vendor/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="<?php echo base_url()?>assets/vendor/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css">
   <link rel="stylesheet" href="<?php echo base_url()?>assets/vendor/vendors/iconfonts/simple-line-icon/css/simple-line-icons.css">
@@ -19,9 +19,10 @@
   <!-- plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
+
+  <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css">
   <link rel="stylesheet" href="<?php echo base_url()?>assets/vendor/css/style.css">
   <!-- endinject -->
   <link rel="shortcut icon" href="<?php echo base_url()?>assets/vendor/images/favicon.png" />
 </head>
-
 <body>
--- application/views/user/login.php
+++ application/views/user/login.php
@@ -10,18 +10,19 @@
           <div class="col-12 col-md-8 h-100 bg-white">
             <div class="auto-form-wrapper d-flex align-items-center justify-content-center flex-column">
               <div class="nav-get-started">
-                <p>Don't have an account?</p>
-                <a class="btn get-started-btn" href="register-2.html">GET STARTED</a>
+                <p class="font">Don't have an account?</p>
+                <a class="btn get-started-btn font" href="<?php echo base_url()?>users/register">GET STARTED</a>
               </div>
-              <form action="#">
-                <h3 class="mr-auto">Hello! let's get started</h3>
-                <p class="mb-5 mr-auto">Enter your details below.</p>
+              <form action="<?php echo base_url()?>users/login" method="POST">
+                <h3 class="mr-auto font">Welcome to the SU Food Ordering System, where ordering meals is made much faster.
+                  <br> <span class='get-started'></span></h3><br>
+                <!-- <p class="mb-5 mr-auto font details">Enter your details below.</p> -->
                 <div class="form-group">
                   <div class="input-group">
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="mdi mdi-account-outline"></i></span>
                     </div>
-                    <input type="text" class="form-control" placeholder="Username">
+                    <input style="font-size:15px;font-family: 'Raleway', sans-serif;" type="text" class="form-control" name="userId" placeholder="ID number" required>
                   </div>
                 </div>
                 <div class="form-group">
@@ -29,10 +30,10 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="mdi mdi-lock-outline"></i></span>
                     </div>
-                    <input type="password" class="form-control" placeholder="Password">
+                    <input style="font-size:15px;font-family: 'Raleway', sans-serif;" type="password" class="form-control input" name="password" placeholder="Password" required>
                   </div>
                 </div>
                 <div class="form-group">
-                  <button class="btn btn-primary submit-btn">SIGN IN</button>
+                  <button class="btn btn-primary submit-btn" name="sign_in">SIGN IN</button>
                 </div>
                 
