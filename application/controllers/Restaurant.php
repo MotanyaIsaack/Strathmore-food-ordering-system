65f166615765610da5a0ec494148e27243f5fa1d
danielolamide <djokogo@gmail.com>
2018-10-02 14:59:24 +0300

    Fixed Model Problems

---
+++ application/controllers/Restaurant.php
@@ -0,0 +1,8 @@
+<?php
+    class Restaurant extends CI_Controller{
+        public function view(){
+            $this->load->view('restaurant/header');
+            // $this->load->view('templates/footer');
+        }
+    }
+?>
---
+++ application/views/restaurant/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Restaurant Module</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="icon" type="image/png" href="<?php echo base_url()?>assets/img/favicon.png">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url()?>assets/css/materialize.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url()?>assets/css/restaurant.css">
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/materialize.css" async></script>
+    <script src="<?php echo base_url()?>assets/js/restaurant.css" async></script>
+    
+</head>
+<body>
+    
+</body>
+</html>
