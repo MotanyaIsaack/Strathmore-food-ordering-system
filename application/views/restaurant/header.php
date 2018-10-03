470b295a041c429585b8af8712a8aa191a24051a
danielolamide <djokogo@gmail.com>
2018-10-03 03:08:19 +0300

    Complete-Restaurant-Module Dashboard Homepage

--- application/views/restaurant/header.php
+++ application/views/restaurant/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Restaurant Module</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
     <link rel="icon" type="image/png" href="<?php echo base_url()?>assets/img/favicon.png">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url()?>assets/css/materialize.css">
