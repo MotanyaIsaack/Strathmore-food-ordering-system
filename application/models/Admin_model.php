7863f50bdf66174a959fbe94a2fed569f9db1d03
MotanyaIsaack <isaackmotanya6@gmail.com>
2018-10-22 16:59:55 +0300

    Updated Administrator Module

---
+++ application/models/Admin_model.php
@@ -0,0 +1,24 @@
+<?php
+    class Admin_model extends CI_Model{
+        //Load the database
+        public function __construct(){
+            $this->load->database();
+        }
+
+        public function register($Enc_Password,$Status){
+            
+            $data = array(
+                'ID' => $this->input->post('ID'),
+                'Name' => $this->input->post('Firstname').' '.$this->input->post('Lastname'),
+                'Email' => $this->input->post('Email'),
+                'Gender' => $this->input->post('Gender'),
+                'Password' => $Enc_Password,
+                'Type' => $this->input->post('Type'),
+                'Status'=> $Status
+            );
+            $insert = $this->db->insert('users',$data);
+            return $insert;
+        }
+    }
+
+?>
---
+++ application/views/admin/templates/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+                © 2018 EatPlicity The Strathmore Food Ordering System
+            </footer>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="<?= base_url();?>assets/admin/assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="<?= base_url();?>assets/admin/assets/plugins/popper/popper.min.js"></script>
+    <script src="<?= base_url();?>assets/admin/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="<?= base_url();?>assets/admin/assets/js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="<?= base_url();?>assets/admin/assets/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="<?= base_url();?>assets/admin/assets/js/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="<?= base_url();?>assets/admin/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="<?= base_url();?>assets/admin/assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="<?= base_url();?>assets/admin/assets/js/custom.min.js"></script>
+    <!-- ============================================================== -->
+    <!-- Style switcher -->
+    <!-- ============================================================== -->
+    <script src="<?= base_url();?>assets/admin/assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+</body>
+
+
+<!-- Mirrored from wrappixel.com/demos/admin-templates/material-pro/material/pages-blank.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 21 Oct 2018 22:11:41 GMT -->
+</html>
